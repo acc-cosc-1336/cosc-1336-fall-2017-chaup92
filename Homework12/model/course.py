02b887897a8ecc16a78a8c5dbc5a8a2731d334bb
chaup92 <31975405+chaup92@users.noreply.github.com>
2017-12-03 16:37:56 -0600

    Delete course.py

--- Homework12/model/course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-
-    def __init__(self, course_id, title, credit_hour, professor):
-
-        self.course_id = course_id
-        self.title = title
-        self.credit_hour = credit_hour
-        self.professor = professor
